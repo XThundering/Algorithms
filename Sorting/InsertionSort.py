e122db2744712aaad1b3e658c3be185e2be3ec1a
XThundering <XThundering@vip.qq.com>
2018-03-24 23:40:50 +0800

    Improve Insertion Sort

--- Sorting/InsertionSort.py
+++ Sorting/InsertionSort.py
@@ -21,16 +21,46 @@ def insertion_sort(arr, n):
                 break
 
 
+# 插入排序写法2
+def insertion_sort2(arr, n):
+    for i in range(1, n):
+        j = i
+        while j > 0 and arr[j] < arr[j-1]:
+            arr[j], arr[j - 1] = arr[j - 1], arr[j]
+            j -= 1
+
+
+# 插入排序优化
+def insertion_sort3(arr, n):
+    for i in range(1, n):
+        element = arr[i]
+
+        # j存储element 应该插入的位置
+        j = i
+        while j > 0 and arr[j-1] > element:
+            arr[j] = arr[j-1]
+            j -= 1
+        arr[j] = element
+
+
 n = 10000
 arr1 = SortTestHelper.generate_random_list(n, 0, n)
 arr2 = arr1[:]
+arr3 = arr1[:]
+arr4 = arr1[:]
 print('n = ' + str(n))
 SortTestHelper.test_sort('Selection Sort', selection_sort, arr1, n)
 SortTestHelper.test_sort('Insertion Sort', insertion_sort, arr2, n)
+SortTestHelper.test_sort('Insertion Sort2', insertion_sort2, arr3, n)
+SortTestHelper.test_sort('Insertion Sort3', insertion_sort3, arr4, n)
 
 n = 20000
 arr1 = SortTestHelper.generate_random_list(n, 0, n)
 arr2 = arr1[:]
+arr3 = arr1[:]
+arr4 = arr1[:]
 print('n = ' + str(n))
 SortTestHelper.test_sort('Selection Sort', selection_sort, arr1, n)
 SortTestHelper.test_sort('Insertion Sort', insertion_sort, arr2, n)
+SortTestHelper.test_sort('Insertion Sort2', insertion_sort2, arr3, n)
+SortTestHelper.test_sort('Insertion Sort3', insertion_sort3, arr4, n)
