e53891a177e49c8c5d67ccf456b2ec9eb3584d63
XThundering <XThundering@vip.qq.com>
2018-03-29 20:39:45 +0800

    Improve Merge Sort

---
+++ Sorting/MergeSort2.py
@@ -0,0 +1,60 @@
+import SortTestHelper
+from Sorting import insertion_sort4, merge_sort
+
+
+# 将arr[l...mid]和arr[mid+1...r]两部分进行归并
+def __merge(arr, left, mid, right):
+    aux = [None] * (right - left + 1)
+
+    for i in range(left, right + 1):
+        aux[i - left] = arr[i]
+
+    # 初始化，i指向左半部分的起始索引位置l；j指向右半部分起始索引位置mid+1
+    i, j = left, mid + 1
+    for k in range(left, right + 1):
+        if i > mid:  # 如果左半部分元素已经全部处理完毕
+            arr[k] = aux[j - left]
+            j += 1
+        elif j > right:  # 如果右半部分元素已经全部处理完毕
+            arr[k] = aux[i - left]
+            i += 1
+        elif aux[i - left] < aux[j - left]:  # 左半部分所指元素<右半部分所指元素
+            arr[k] = aux[i - left]
+            i += 1
+        else:
+            arr[k] = aux[j - left]  # 左半部分所指元素>=右半部分所指元素
+            j += 1
+
+
+def __merge_sort2(arr, left, right):
+
+    # 对于小规模数组，使用插入排序
+    if (right - left) <= 15:
+        insertion_sort4(arr, left, right)
+        return
+
+    mid = (left + right) // 2
+    __merge_sort2(arr, left, mid)
+    __merge_sort2(arr, mid+1, right)
+
+    if arr[mid] > arr[mid + 1]:
+        __merge(arr, left, mid, right)
+
+
+def merge_sort2(arr, n):
+    __merge_sort2(arr, 0, n-1)
+
+
+n = 100000
+arr1 = SortTestHelper.generate_random_list(n, 0, n)
+arr2 = arr1[:]
+print('n = ' + str(n))
+SortTestHelper.test_sort('Merge Sort', merge_sort, arr1, n)
+SortTestHelper.test_sort('Merge Sort2', merge_sort2, arr2, n)
+
+n = 200000
+arr1 = SortTestHelper.generate_random_list(n, 0, n)
+arr2 = arr1[:]
+print('n = ' + str(n))
+SortTestHelper.test_sort('Selection Sort', merge_sort, arr1, n)
+SortTestHelper.test_sort('Insertion Sort', merge_sort2, arr2, n)
--- Sorting/Sorting.py
+++ Sorting/Sorting.py
@@ -64,6 +64,17 @@ def insertion_sort3(arr, n):
         arr[j] = element
 
 
+# 对arr[l...r]范围的数组进行插入排序
+def insertion_sort4(arr, left, right):
+    for i in range(left + 1, right + 1):
+        element = arr[i]
+        j = i
+        while j > left and arr[j - 1] > element:
+            arr[j] = arr[j - 1]
+            j -= 1
+        arr[j] = element
+
+
 # 冒泡排序
 def bubble_sort(arr, n):
     swapeed = True
@@ -90,3 +101,61 @@ def bubble_sort2(arr, n):
                 # 记录最后一次交换位置，此处之后的元素下一次扫描不考虑
                 new_n = i
         n = new_n
+
+
+# 将arr[l...mid]和arr[mid+1...r]两部分进行归并
+def __merge(arr, left, mid, right):
+    aux = [None] * (right - left + 1)
+
+    for i in range(left, right + 1):
+        aux[i - left] = arr[i]
+
+    # 初始化，i指向左半部分的起始索引位置l；j指向右半部分起始索引位置mid+1
+    i, j = left, mid + 1
+    for k in range(left, right + 1):
+        if i > mid:  # 如果左半部分元素已经全部处理完毕
+            arr[k] = aux[j - left]
+            j += 1
+        elif j > right:  # 如果右半部分元素已经全部处理完毕
+            arr[k] = aux[i - left]
+            i += 1
+        elif aux[i - left] < aux[j - left]:  # 左半部分所指元素<右半部分所指元素
+            arr[k] = aux[i - left]
+            i += 1
+        else:
+            arr[k] = aux[j - left]  # 左半部分所指元素>=右半部分所指元素
+            j += 1
+
+
+# 递归使用归并排序,对arr[l...r]的范围进行排序
+def __merge_sort(arr, left, right):
+    if left >= right:
+        return
+
+    mid = (left + right) // 2
+    __merge_sort(arr, left, mid)
+    __merge_sort(arr, mid+1, right)
+    __merge(arr, left, mid, right)
+
+
+def merge_sort(arr, n):
+    __merge_sort(arr, 0, n-1)
+
+
+def __merge_sort2(arr, left, right):
+
+    # 对于小规模数组，使用插入排序
+    if (right - left) <= 15:
+        insertion_sort4(arr, left, right)
+        return
+
+    mid = (left + right) // 2
+    __merge_sort2(arr, left, mid)
+    __merge_sort2(arr, mid+1, right)
+
+    if arr[mid] > arr[mid + 1]:
+        __merge(arr, left, mid, right)
+
+
+def merge_sort2(arr, n):
+    __merge_sort2(arr, 0, n-1)
