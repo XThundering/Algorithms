8633367a42ab4fc5e602167651e16f737a6336ae
XThundering <XThundering@vip.qq.com>
2018-03-29 21:29:59 +0800

    Add some quick sort test method

--- Sorting/QuickSort.py
+++ Sorting/QuickSort.py
@@ -1,3 +1,4 @@
+import sys
 import SortTestHelper
 from Sorting import merge_sort, merge_sort2
 
@@ -29,7 +30,8 @@ def quick_sort(arr, n):
     __quick_sort(arr, 0, n-1)
 
 
-n = 100000
+# 测试1 一般测试
+n = 10000
 arr1 = SortTestHelper.generate_random_list(n, 0, n)
 arr2 = arr1[:]
 arr3 = arr1[:]
@@ -38,8 +40,11 @@ SortTestHelper.test_sort('Merge Sort', merge_sort, arr1, n)
 SortTestHelper.test_sort('Merge Sort2', merge_sort2, arr2, n)
 SortTestHelper.test_sort('Quick Sort', quick_sort, arr3, n)
 
-n = 200000
-arr1 = SortTestHelper.generate_random_list(n, 0, n)
+# 测试2 测试近乎有序的数组
+# 对于近乎有序的数组, 快速排序算法退化成了O(n^2)级别的算法
+sys.setrecursionlimit(n)  # 设置Python的最大递归深度为n，因为在近乎有序的列表中，快排递归深度非常大
+swap_times = 100
+arr1 = SortTestHelper.generate_nearly_ordered_list(n, swap_times)
 arr2 = arr1[:]
 arr3 = arr1[:]
 print('n = ' + str(n))
